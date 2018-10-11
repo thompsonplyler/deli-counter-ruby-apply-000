8de7303dfd0222ce8cae5303564d31991c660b46
Thompson Plyler <nealok.g@gmail.com>
2018-10-11 14:43:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,9 +13,8 @@ def line(deli)
   end
 end
 
-def take_a_number(deli,name)
-  deli.push(name)
-  puts "Welcome, #{name}. You are number #{deli.length} in line."
+def take_a_number(deli)
+  puts "Welcome, You are number #{deli.length} in line."
 end
 
 def now_serving(deli)
